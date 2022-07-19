59ca3e4d84c6a9a96cf22789c0f71e5bac02350f
Lorenzo Bernardi <lorenzo.bernardi@llr.in2p3.fr>
2022-07-19 17:26:25 +0200

    crée un setup.py fonctionnel

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+      name='LTC2977_duino',
+      version='0.1.a',
+      description='Module to interact with LTM2877 through Linduino',
+      author='Lorenzo Bernardi',
+      author_email='bernardi@llr.in2p3.fr',
+      #url='https://www.python.org/sigs/distutils-sig/',
+      install_requires=['pyserial'],
+      license="MIT",
+      packages=find_packages(),
+      package_data={'LTC2977_duino': ['ltc2977.desc']},
+     )
